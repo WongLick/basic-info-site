c8b44aa360f5c78d58f22e4ddcd4901a750c6392
WongLick <163981891+WongLick@users.noreply.github.com>
2024-09-27 12:13:28 +0800

    Express
    - modify into express code

--- index.js
+++ index.js
@@ -1,34 +1,81 @@
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
+//FIRST
+// const http = require("http");
+// const url = require("url");
+// const fs = require("fs");
+// const path = require("path");
+// const express = require("express");
+// const app = express();
+
+
+// const routes = {
+//   "/": "./index.html",
+//   "/about": "./about.html",
+//   "/contact-me": "./contact-me.html",
+// };
+
+// const server = http.createServer((req, res) => {
+//   const parsedUrl = url.parse(req.url, true);
+//   const pathname = parsedUrl.pathname;
+
+//   //default to 404 if route not found
+//   const filename = routes[pathname] || "./404.html";
+//   const contentType = "text/html";
+
+//   fs.readFile(filename, (err, data) => {
+//     if (err) {
+//       res.writeHead(404, { "Content-Type": "text/html" });
+//       return res.end("404 Not Found");
+//     }
+//     res.writeHead(200, { "Content-Type": contentType });
+//     res.write(data);
+//     return res.end();
+//   });
+// });
+
+// const PORT = 8080;
+// server.listen(PORT, () => {
+//   console.log(`Server running at http://localhost:${PORT}`);
+// });
+
+//EXPRESS
+const express = require("express");
 const path = require("path");
 
-const routes = {
-  "/": "./index.html",
-  "/about": "./about.html",
-  "/contact-me": "./contact-me.html",
-};
+const app = express();
 
-const server = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-  const pathname = parsedUrl.pathname;
-
-  //default to 404 if route not found
-  const filename = routes[pathname] || "./404.html";
-  const contentType = "text/html";
-
-  fs.readFile(filename, (err, data) => {
-    if (err) {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      return res.end("404 Not Found");
-    }
-    res.writeHead(200, { "Content-Type": contentType });
-    res.write(data);
-    return res.end();
-  });
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"), (err) => {
+        if (err) {
+            console.error("Error sending index.html:", err);
+            res.status(500).send("Internal Server Error");
+        }
+    });
 });
 
-const PORT = 8080;
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "about.html"), (err) => {
+        if (err) {
+            console.error("Error sending about.html:", err);
+            res.status(500).send("Internal Server Error");
+        }
+    });
 });
+
+app.get("/contact-me", (req, res) => {
+    res.sendFile(path.join(__dirname, "contact-me.html"), (err) => {
+        if (err) {
+            console.error("Error sending contact-me.html:", err);
+            res.status(500).send("Internal Server Error");
+        }
+    });
+});
+
+// 404 Handling
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`My info site wooo, on port ${PORT}`));
+
+
